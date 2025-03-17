b7e886da9e67af5cede034c755d3ac80755f902d
Harry Lee <haewon.harry.lee@gmail.com>
2025-03-16 19:30:30 -0600

    added generic primary button tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-between p-10 min-h-screen max-w-sm">
-        <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-          <div>
-            <Image
-              src={Logo3}
-              alt="Savket Logo"
-              width={300}
-              height={300}
-              className="object-contain"
-              priority
-            />
-          </div>
-          <h1 className="text-4xl">Savket</h1>
-          <h2 className="text-2xl">Welcome to Savket!</h2>
+    <div className="flex flex-col gap-10 mx-auto justify-between p-10 min-h-screen max-w-sm">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <div>
+          <Image
+            src={Logo3}
+            alt="Savket Logo"
+            width={300}
+            height={300}
+            className="object-contain"
+            priority
+          />
         </div>
-        <div className="flex flex-col items-center gap-3 w-full">
-          <Link href="/create-account"
-            className="w-full bg-lime-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-lime-400 transition-colors"
-          >Start</Link>
-          <div className="flex gap-2">
-            <span>Do you already have an account?</span>
-            <Link href="/login" className="hover:underline">Login</Link>
-          </div>
+        <h1 className="text-4xl">Savket</h1>
+        <h2 className="text-2xl">Welcome to Savket!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link href="/create-account"
+          className="primary-btn py-2 text-lg"
+        >Start</Link>
+        <div className="flex gap-2">
+          <span>Do you already have an account?</span>
+          <Link href="/login" className="hover:underline">Login</Link>
         </div>
       </div>
     </div>
