a4cc99a50cc08e6381930f02a9ae82ceb91693e9
Harry Lee <haewon.harry.lee@gmail.com>
2025-03-24 23:16:14 -0600

    added images.remotePatterns for supabase storage domain to use photo urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // register supabase storage domain
+    // option 1) remotePatterns
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cyumfhjuozcbgmbmqoek.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+
+    // option 2) domains
+    // domains: ['cyumfhjuozcbgmbmqoek.supabase.co'],
+  },
+};
 
 export default nextConfig;
